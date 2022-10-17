11f0545dd4e093a143bbe5e32ade0e3473b42887
pritesh-vss <pritesh.bavaskar@vishleshan.net>
2022-10-17 15:21:56 +0530

    Moved Icon Button Component to common

--- dist/index.js
+++ dist/index.js
@@ -9,5 +9,12 @@ Object.defineProperty(exports, "Button", {
     return _Button.default;
   }
 });
+Object.defineProperty(exports, "IconButton", {
+  enumerable: true,
+  get: function get() {
+    return _IconButton.default;
+  }
+});
 var _Button = _interopRequireDefault(require("./Button/Button"));
+var _IconButton = _interopRequireDefault(require("./IconButton/IconButton"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
